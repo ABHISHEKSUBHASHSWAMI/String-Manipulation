ba5bda25a41112c9b1754d4e3070708474bbe25c
S ABHISHEK <79701277+ABHISHEKSUBHASHSWAMI@users.noreply.github.com>
2022-03-31 22:26:54 +0530

    Update str10.py
    Program to change a given string to a new string where the first and last chars have been exchanged.

--- str10.py
+++ str10.py
@@ -1,9 +1,9 @@
 #Program to change a given string to a new string where the first and last chars have been exchanged.
 
 string=str(input("Enter a string :"))
-first=string[0]
-last=string[-1]
+first=string[0]                             #store first index element of string in variable
+last=string[-1]                             #store last index element of string in variable
 
-new=last+string[1:-1]+first
+new=last+string[1:-1]+first                 #concatenate last the middle part and first part
 print(new)
 
