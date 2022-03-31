e821e5e804e3f75d07ca0d0bf6138f387fb8e80c
S ABHISHEK <79701277+ABHISHEKSUBHASHSWAMI@users.noreply.github.com>
2022-03-31 22:58:27 +0530

    Create str18.py
    Program to check whether a string starts with specified characters

---
+++ str18.py
@@ -0,0 +1,5 @@
+# Program to check whether a string starts with specified characters
+
+temp=str(input("Enter a string :"))
+check=str(input("Enter starting characters :"))
+print(temp.startswith(check))
