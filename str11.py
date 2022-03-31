86cdfdf47fe4abbb1279420bcfa5a84a11a56c87
S ABHISHEK <79701277+ABHISHEKSUBHASHSWAMI@users.noreply.github.com>
2022-03-31 22:20:29 +0530

    Update str11.py
    Write a Python program to count the occurrences of each word in a given sentence.

--- str11.py
+++ str11.py
@@ -1,13 +1,13 @@
 # Write a Python program to count the occurrences of each word in a given sentence.
 
 string=str(input("Enter a string :"))
-words=string.split()
-count=dict()
+words=string.split()                                #store splitted string into variable
+count=dict()                                        #initiate a dictionary
 
-for word in words:
-    if(word in count):
-        count[word]=count[word]+1
-    else:
+for word in words:                                  #initiate loop
+    if(word in count):                              #if word is already in dictionary 
+        count[word]=count[word]+1                   #increment the count
+    else:                                           #else keep it 1
         count[word]=1
     
 print(count)
