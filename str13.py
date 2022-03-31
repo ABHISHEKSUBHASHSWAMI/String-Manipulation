eaa121a7e0da427de55d415e70b3e18edfd065d0
S ABHISHEK <79701277+ABHISHEKSUBHASHSWAMI@users.noreply.github.com>
2022-03-31 22:29:19 +0530

    Create str13.py
    Program that takes input from the user and displays that input back in upper and lower cases

---
+++ str13.py
@@ -0,0 +1,5 @@
+#Program that takes input from the user and displays that input back in upper and lower cases
+
+string=str(input("Enter a string :"))
+
+print("Upper case :",string.upper(),"\nLower case :",string.lower())      #show upper and lower cases using methods.
